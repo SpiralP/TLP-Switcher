bc5077295367e7e8c3839effd359f1b90f5634c7
SpiralP <spiral.palmer@gmail.com>
2019-04-09 09:20:09 -0700

    formatting

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ TLP-Switcher is very basic gnome shell extension for switching TLP configuration
 ![Screenshot](https://cloud.githubusercontent.com/assets/9300522/25596313/7c3639b6-2e96-11e7-87be-4ad536d5c38b.png)
 
 ## Dependencies
+
 - TLP
 
 ## Install
+
 Download and extract zip to `~/.local/share/gnome-shell/extensions/`, and restart gnome shell.
 
 or
@@ -26,9 +28,10 @@ or
 
 `cp /etc/default/tlp ~/.tlp/MyProfile`
 
-Selecting a profile will prompt you for root access to run [tlp_update.sh](tlp_update.sh). This script copys your profile to `/etc/default/tlp` and runs `tlp start`. 
+Selecting a profile will prompt you for root access to run [tlp_update.sh](tlp_update.sh). This script copys your profile to `/etc/default/tlp` and runs `tlp start`.
 
 Changes to `~/.tlp/` (adding/removing profiles) are reflected (not applied) automatically.
 
 ## Gnome Version
-This extension is built and tested under gnome 3.24.1. It *may* work on previous versions of gnome but the metadata.json file only lists 3.24+. You can simply edit it and add your gnome version (if it works fine, leave me a comment so I can update the metadata.json).
+
+This extension is built and tested under gnome 3.24.1. It _may_ work on previous versions of gnome but the metadata.json file only lists 3.24+. You can simply edit it and add your gnome version (if it works fine, leave me a comment so I can update the metadata.json).
--- extension.js
+++ extension.js
@@ -7,142 +7,150 @@ const GLib = imports.gi.GLib;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Util = imports.misc.util;
 
-const PROFILE_DIR = '/.tlp/';
-		
-const TLPButton = new Lang.Class({
-	Name: 'TLPButton.button',
-	Extends: PanelMenu.Button,
-	
-	_init: function() {
-		this.parent(0.0, 'TLP Switcher');
-		
-		// panel icon
-		
-		this.actor.add_actor(
-			new St.Icon({
-				icon_name: 'applications-science-symbolic',
-				style_class: 'system-status-icon'
-			}));
-			
-		// popup menu title
-		
-		let itemTitle = new PopupMenu.PopupMenuItem('TLP Profile');
-		itemTitle.actor.reactive = false;
-		
-		let menuTitle = new PopupMenu.PopupMenuSection();
-		menuTitle.addMenuItem(itemTitle);
-		
-		this.menu.addMenuItem(menuTitle);
-		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		
-		// popup menu profiles
-		
-		this._menuProfiles = new PopupMenu.PopupMenuSection();
-		this.menu.addMenuItem(this._menuProfiles);
-		
-		this._itemProfiles = null;
-		this._profileDir = GLib.get_home_dir() + PROFILE_DIR;
-	},
-	
-	_onOpenStateChanged: function(menu, open) {
-		if (open)
-			this._updateProfiles();
-			
-		this.parent(menu, open);
-	},
-	
-	_updateProfiles: function() {
+const PROFILE_DIR = "/.tlp/";
 
-		// clear existing
-		
-		this._menuProfiles.removeAll();
-		
-		// list profiles
-	
-		let output = GLib.spawn_command_line_sync('ls ' + this._profileDir);
-		this._profiles = output[1].toString().split('\n');
-		this._profiles.pop();
-		
-		// create directory if it doesn't exist
-		
-		if (output[2].toString().indexOf('No such file or directory') != -1) 
-			GLib.spawn_command_line_async('mkdir ' + this._profileDir);
-		
-		let len = this._profiles.length;
-		if (len > 0) {
-		
-			// construct profiles menu
-			
-			this._itemProfiles = [];
-			let p;
-			for (let i = 0; i < len; ++i) {
-				p = new PopupMenu.PopupMenuItem(this._profiles[i]);
-				p.id = i;
-				p.connect('activate', Lang.bind(this, function(actor, event) {
-					this._activate(actor.id);
-				}));
-				
-				this._itemProfiles[i] = p;
-				this._menuProfiles.addMenuItem(p);
-			}
-			
-			// determine current profile
-			
-			this._checkActive();
-		}
-		else {
-		
-			// no profiles
-		
-			let item = new PopupMenu.PopupMenuItem('No profiles in ~/.tlp');
-			item.actor.reactive = false;
-			this._menuProfiles.addMenuItem(item);
-		}
-	},
-	
-	_activate: function(index) {
-		
-		// update active ornament
-		
-		for (let i = 0; i < this._itemProfiles.length; ++i)
-			this._itemProfiles[i].setOrnament(PopupMenu.Ornament.NONE);
-		this._itemProfiles[index].setOrnament(PopupMenu.Ornament.DOT);
-		
-		// run tlp update script
-		// (cp script /etc/default/tlp && tlp start)
-		
-		let script = Me.dir.get_path() + '/tlp_update.sh';
-		let [parsed, args] = 
-			GLib.shell_parse_argv('/usr/bin/pkexec /bin/bash '.concat(script, ' \'', this._profileDir, this._profiles[index], '\''));
-		if (parsed)
-			Util.spawn(args);
-	},
-	
-	_checkActive: function() {
-		if (this._profiles.length == 0)
-			return;
-			
-		let config = GLib.spawn_command_line_sync('tlp-stat -c')[1].toString().split('\n');
-		let profile;
-		
-		for (let i = 0; i < this._profiles.length; ++i) {
-			profile = GLib.spawn_command_line_sync('cat \''.concat(this._profileDir, this._profiles[i], '\''))[1].toString().split('\n');
-			
-			if (this._profileMatch(config, profile)) {
-				this._itemProfiles[i].setOrnament(PopupMenu.Ornament.DOT);
-				break;
-			}
-		}
-	},
-	
-	_profileMatch: function(config, profile) {
-		for (let i = 3; i < config.length; ++i) {
-			if (config[i] && profile.indexOf(config[i]) == -1)
-				return false;
-		}
-		
-		return true;
-	}
+const TLPButton = new Lang.Class({
+  Name: "TLPButton.button",
+  Extends: PanelMenu.Button,
+
+  _init: function() {
+    this.parent(0.0, "TLP Switcher");
+
+    // panel icon
+
+    this.actor.add_actor(
+      new St.Icon({
+        icon_name: "applications-science-symbolic",
+        style_class: "system-status-icon",
+      })
+    );
+
+    // popup menu title
+
+    let itemTitle = new PopupMenu.PopupMenuItem("TLP Profile");
+    itemTitle.actor.reactive = false;
+
+    let menuTitle = new PopupMenu.PopupMenuSection();
+    menuTitle.addMenuItem(itemTitle);
+
+    this.menu.addMenuItem(menuTitle);
+    this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+    // popup menu profiles
+
+    this._menuProfiles = new PopupMenu.PopupMenuSection();
+    this.menu.addMenuItem(this._menuProfiles);
+
+    this._itemProfiles = null;
+    this._profileDir = GLib.get_home_dir() + PROFILE_DIR;
+  },
+
+  _onOpenStateChanged: function(menu, open) {
+    if (open) this._updateProfiles();
+
+    this.parent(menu, open);
+  },
+
+  _updateProfiles: function() {
+    // clear existing
+
+    this._menuProfiles.removeAll();
+
+    // list profiles
+
+    let output = GLib.spawn_command_line_sync("ls " + this._profileDir);
+    this._profiles = output[1].toString().split("\n");
+    this._profiles.pop();
+
+    // create directory if it doesn't exist
+
+    if (output[2].toString().indexOf("No such file or directory") != -1)
+      GLib.spawn_command_line_async("mkdir " + this._profileDir);
+
+    let len = this._profiles.length;
+    if (len > 0) {
+      // construct profiles menu
+
+      this._itemProfiles = [];
+      let p;
+      for (let i = 0; i < len; ++i) {
+        p = new PopupMenu.PopupMenuItem(this._profiles[i]);
+        p.id = i;
+        p.connect(
+          "activate",
+          Lang.bind(this, function(actor, event) {
+            this._activate(actor.id);
+          })
+        );
+
+        this._itemProfiles[i] = p;
+        this._menuProfiles.addMenuItem(p);
+      }
+
+      // determine current profile
+
+      this._checkActive();
+    } else {
+      // no profiles
+
+      let item = new PopupMenu.PopupMenuItem("No profiles in ~/.tlp");
+      item.actor.reactive = false;
+      this._menuProfiles.addMenuItem(item);
+    }
+  },
+
+  _activate: function(index) {
+    // update active ornament
+
+    for (let i = 0; i < this._itemProfiles.length; ++i)
+      this._itemProfiles[i].setOrnament(PopupMenu.Ornament.NONE);
+    this._itemProfiles[index].setOrnament(PopupMenu.Ornament.DOT);
+
+    // run tlp update script
+    // (cp script /etc/default/tlp && tlp start)
+
+    let script = Me.dir.get_path() + "/tlp_update.sh";
+    let [parsed, args] = GLib.shell_parse_argv(
+      "/usr/bin/pkexec /bin/bash ".concat(
+        script,
+        " '",
+        this._profileDir,
+        this._profiles[index],
+        "'"
+      )
+    );
+    if (parsed) Util.spawn(args);
+  },
+
+  _checkActive: function() {
+    if (this._profiles.length == 0) return;
+
+    let config = GLib.spawn_command_line_sync("tlp-stat -c")[1]
+      .toString()
+      .split("\n");
+    let profile;
+
+    for (let i = 0; i < this._profiles.length; ++i) {
+      profile = GLib.spawn_command_line_sync(
+        "cat '".concat(this._profileDir, this._profiles[i], "'")
+      )[1]
+        .toString()
+        .split("\n");
+
+      if (this._profileMatch(config, profile)) {
+        this._itemProfiles[i].setOrnament(PopupMenu.Ornament.DOT);
+        break;
+      }
+    }
+  },
+
+  _profileMatch: function(config, profile) {
+    for (let i = 3; i < config.length; ++i) {
+      if (config[i] && profile.indexOf(config[i]) == -1) return false;
+    }
+
+    return true;
+  },
 });
 
 function init() {}
@@ -150,11 +158,10 @@ function init() {}
 let button;
 
 function enable() {
-	button = new TLPButton;
-	Main.panel.addToStatusArea('ID-TLPSwitcher', button);
+  button = new TLPButton();
+  Main.panel.addToStatusArea("ID-TLPSwitcher", button);
 }
 
 function disable() {
-	button.destroy();
+  button.destroy();
 }
-
